daec901366e77b58a9239a7ce0c32a3350c0e85d
Don Park <donp@donp.org>
2013-09-03 16:06:59 -0700

    building yet another wopr

--- warpbubble/ruby/lib/warp_bubble.rb
+++ warpbubble/ruby/lib/warp_bubble.rb
@@ -1,8 +1,14 @@
 require 'httparty'
 require 'redis'
 
+Dir['warp_bubble/*'].each{|filename| require filename}
+
 class WarpBubble
   @@version = "0.01"
 
   def self.version; @@version; end
+
+  def initialize
+    puts ""
+  end
 end
---
+++ warpbubble/ruby/lib/warp_bubble/market_monitor.rb
@@ -0,0 +1,4 @@
+class WarpBubble
+  class MarketMonitor
+  end
+end
