72002581705a10ff3ce7cb57483a5ff8f83b701a
Don Park <donp@donp.org>
2013-09-04 11:14:29 -0700

    poll if the data is old

--- warpbubble/nodejs/market_monitor.js
+++ warpbubble/nodejs/market_monitor.js
@@ -24,6 +24,7 @@ redis_sub.on('message', function(channel, data){
 })
 
 redis_sub.subscribe(wb_channel)
+//redis.publish(wb_channel, '{"action":"setup"}')
 
 var exchange_roster = {}
 
@@ -31,11 +32,35 @@ function setup(packet){
   console.log('** Setup')
   db.get('exchange_list', function(value){
     value.forEach(function(exchange_name){
-      exchange_roster[exchange_name] = {}
+      exchange_roster[exchange_name] = {name:exchange_name}
     })
     console.dir(exchange_roster)
   })
 }
 
-function time(packet){}
+function time(packet){
+  console.log("time! "+JSON.stringify(packet))
+  Object.keys(exchange_roster).forEach(function(exchange_name){
+    var exchange = exchange_roster[exchange_name]
+    older_than(exchange, 60, function(){
+      poll(exchange)
+    })
+  })
+}
 
+
+function older_than(exchange, max_age, cb){
+  var age;
+  if(exchange.time){
+    age =  exchange.time - (new Date())
+  } else {
+    age = 1000
+  }
+  if(age > max_age) {
+    cb()
+  }
+}
+
+function poll(exchange){
+  console.log("polling "+exchange["name"])
+}
