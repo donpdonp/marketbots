b1eabea57df68c85a609f8aad53f9a3109fd64d1
Don Park <donp@donp.org>
2013-09-05 11:09:13 -0700

    listen to redis

--- warpbubble/ruby/lib/warp_bubble.rb
+++ warpbubble/ruby/lib/warp_bubble.rb
@@ -14,15 +14,37 @@ class WarpBubble
     @@services.push(descriptor)
   end
 
+  @@channel_name = 'warp_bubble'
   @@redis = Redis.new
+
+  def initialize
+    Thread.new do
+      listen
+    end
+  end
+
+  def listen
+    Redis.new.subscribe(@@channel_name) do |on|
+      on.subscribe do |channel, count|
+      end
+
+      on.message do |channel, json|
+        message = JSON.parse(json)
+        puts message.inspect
+      end
+    end
+  end
 
   def mainloop
     puts "Services: #{WarpBubble.services.map{|s| s["name"]}.inspect}"
     puts "Issuing setup"
-    # fix
-    @@redis.publish('warp_bubble', '{"action":"warp_bubble setup"}')
+    publish({"action" => "warp_bubble setup"})
 
     WarpBubble.services.map{|t| t["thread"].join}
+  end
+
+  def publish(msg)
+    @@redis.publish(@@channel_name, msg.to_json)
   end
 end
 
