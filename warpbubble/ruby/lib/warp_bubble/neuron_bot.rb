d562617a897df21692d6571f2e2da2aae84d8e7b
Don Park <donp@donp.org>
2013-10-07 14:00:09 -0700

    use the bot word ordering

--- warpbubble/ruby/lib/warp_bubble/neuron_bot.rb
+++ warpbubble/ruby/lib/warp_bubble/neuron_bot.rb
@@ -10,7 +10,7 @@ class WarpBubble
           @wb.publish({"action" => "balance refresh", "payload" => {"exchange" => "*"}})
         elsif match = payload["message"].match(/^ready/)
           @wb.publish({"action" => "balance ready", "payload" => {}})
-        elsif match = payload["message"].match(/^drop plan/)
+        elsif match = payload["message"].match(/^plan drop/)
           @wb.publish({"action" => "plan drop", "payload" => {}})
         elsif match = payload["message"].match(/^(\w+)\s+(\w+)\s+(\w+)/)
           @wb.publish({"action" => "#{match[2]} #{match[3]}", "payload" => {"exchange" => match[1]}})
