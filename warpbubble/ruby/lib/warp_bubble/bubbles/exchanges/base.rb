80463e445aeb7eb26e8a3b4aa7cf51e5022170e2
Don Park <donp@donp.org>
2013-10-08 07:08:19 -0700

    email, but no confirmation email

--- warpbubble/ruby/lib/warp_bubble/bubbles/exchanges/base.rb
+++ warpbubble/ruby/lib/warp_bubble/bubbles/exchanges/base.rb
@@ -77,9 +77,14 @@ class WarpBubble
         if items
           items = [items] unless items.is_a?(Array)
           confirm_email = items.select{|i| i["title"].match(title_words)}.last
-          @@driver.navigate.to(confirm_email["rdf:about"])
-          @@driver.find_elements(:css, 'div.mailview a').reject do |link|
-            link.attribute('href').match(link_words)
+          if confirm_email
+            @@driver.navigate.to(confirm_email["rdf:about"])
+            @@driver.find_elements(:css, 'div.mailview a').reject do |link|
+              link.attribute('href').match(link_words)
+            end
+          else
+            log "no confirmation mail for #{url}"
+            []
           end
         else
           log "no mail for #{url}"
