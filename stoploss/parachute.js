09cb71962019478e523337431e1eb7835d91bba7
Don Park <donp@donp.org>
2013-11-29 14:35:48 -0800

    confusing names

--- stoploss/parachute.js
+++ stoploss/parachute.js
@@ -111,7 +111,7 @@ function trade_decision(price){
           buy(price)
         } else {
           json_log({swing:"ARMED. waiting above $"+buy_swing.toFixed(2),
-                    low_water: low_water, sold_at: sell_price, buy_price: buy_price})
+                    low_water: low_water, sold_at: sell_price, abort_price: buy_price})
         }
       } else {
         json_log({swing:"dead between sell and abort.", low_water: low_water,
