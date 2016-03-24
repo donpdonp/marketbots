401a42876bc1f395e113903cc14a9969acc34df9
Don Park <donp@donp.org>
2016-03-24 10:30:00 -0700

    signal log

--- wsarbi/server.cr
+++ wsarbi/server.cr
@@ -72,11 +72,11 @@ redis.subscribe("orderbook") do |on|
       earn = orderbook.arbitrage(win_bid, win_ask)
       profit = earn - spend
       profit_percent = profit/spend*100
-      if profit_percent >= 0.1
+      if profit_percent >= 0.01
         low_ask = orderbook.asks.bins.first.offers.first
         high_bid = orderbook.bids.bins.first.offers.first
-        puts "#### ARBITRAGE #{"%0.8f" % profit}btc #{"%0.2f" % (profit_percent)}% of #{spend}btc buy #{low_ask.exchange} sell #{high_bid.exchange}"
-        File.open("arb.out", "a") { |f| f.puts "#{Time.now} #{"%0.8f" % profit}btc #{"%0.2f" % (profit/spend*100)}% of #{spend}" }
+        puts "#### ARBITRAGE #{"%0.8f" % profit}btc #{"%0.2f" % (profit_percent)}% of #{spend}btc"
+        File.open("signal.log", "a") { |f| f.puts "#{Time.now} #{"%0.8f" % profit}btc #{"%0.2f" % (profit/spend*100)}% of #{spend}  buy #{low_ask.exchange} sell #{high_bid.exchange}" }
         puts "Arbitrage ask value #{"%0.8f" % win_ask.value}btc  bid value #{"%0.8f" % win_bid.value}btc"
         win_ask.bins.each do |ob|
           puts "      ASK |#{ob.exchanges}| bin #{ob.price.to_s} x#{ob.quantity}"
