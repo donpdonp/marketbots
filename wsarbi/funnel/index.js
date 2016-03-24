13801799c1da0251f18d09910de2cc31cb74f362
Don Park <donp@donp.org>
2016-03-24 10:24:55 -0700

    full hose

--- wsarbi/funnel/index.js
+++ wsarbi/funnel/index.js
@@ -12,10 +12,10 @@ var redis = require('redis').createClient()
 var channel_name = 'orderbook'
 
 
-//stream_setup()
+stream_setup()
 
 // Poloniex stream
-//poloniex_stream.open()
+poloniex_stream.open()
 
 // Kraken pump
 setInterval(kraken_refresh, 5000)
