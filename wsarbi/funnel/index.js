cd1be7534b737b95cdba3a445f6cefc7184e9528
Don Park <donp@donp.org>
2016-03-24 09:42:59 -0700

    js shuffle

--- wsarbi/funnel/index.js
+++ wsarbi/funnel/index.js
@@ -4,7 +4,7 @@ var bws = new BitfinexWS();
 var request = require('request')
 
 var autobahn = require('autobahn');
-var connection = new autobahn.Connection({
+var poloniex_stream = new autobahn.Connection({
   url: "wss://api.poloniex.com",
   realm: "realm1"
 });
@@ -12,76 +12,88 @@ var connection = new autobahn.Connection({
 var redis = require('redis').createClient()
 var channel_name = 'orderbook'
 
-// poloniex pump
-connection.onopen = function (session) {
-  function marketEvent (args, kwargs) {
-    args.forEach(function(ob){
-      if(ob.type == "orderBookModify" || ob.type == "orderBookRemove") {
-        console.log("POLO", ob.type, ob.data);
-        wsob = {
-          exchange: 'poloniex',
-          market: 'ETH:BTC',
-          type:   ob.data.type,
-          price:  ob.data.rate,
-          amount: ob.type == "orderBookRemove" ? "0" : ob.data.amount
-        }
-        redis.publish(channel_name, JSON.stringify(wsob))
-      }
-    })
-  }
-  function tickerEvent (args,kwargs) {
-  }
-  function trollboxEvent (args,kwargs) {
-  }
 
-  session.subscribe('BTC_ETH', marketEvent);
-  //session.subscribe('ticker', tickerEvent);
-  //session.subscribe('trollbox', trollboxEvent);
-}
+stream_setup()
 
-connection.open()
+// Poloniex stream
+poloniex_stream.open()
 
-
-// Bitfinex pump
-bws.on('open', function () {
-  bws.subscribeOrderBook('ETHBTC');
-
-  //bws.subscribeTrades('ETHBTC');
-  //bws.subscribeTicker('LTCBTC');
-});
-
-bws.on('close', function (pair, trade) {
-    console.log('btfx closed.', trade);
-});
-
-bws.on('trade', function (pair, trade) {
-    console.log('Trade:', trade);
-});
-
-bws.on('orderbook', function (pair, book) {
-  console.log('BTFX', book);
-  wsob = {
-    exchange: 'bitfinex',
-    market: 'ETH:BTC',
-    type:   book.amount > 0 ? "bid" : "ask",
-    price:  ""+book.price, // str to float to str, crosses fingers
-    amount: book.count == 0 ? "0" : (""+Math.abs(book.amount))
-  }
-  redis.publish(channel_name, JSON.stringify(wsob))
-});
-
-bws.on('ticker', function (pair, ticker) {
-    console.log('Ticker:', ticker);
-});
-
-bws.on('subscribed', function (data) {
-    console.log('New subscription', data);
-});
-
-bws.on('error', console.error);
-
+// Kraken pump
+setInterval(kraken_refresh, 5000)
 // Bleutrade pump
 setInterval(bleu_refresh, 5000)
+
+
+function stream_setup() {
+
+  // poloniex pump
+  poloniex_stream.onopen = function (session) {
+    function marketEvent (args, kwargs) {
+      args.forEach(function(ob){
+        if(ob.type == "orderBookModify" || ob.type == "orderBookRemove") {
+          console.log("POLO", ob.type, ob.data);
+          wsob = {
+            exchange: 'poloniex',
+            market: 'ETH:BTC',
+            type:   ob.data.type,
+            price:  ob.data.rate,
+            amount: ob.type == "orderBookRemove" ? "0" : ob.data.amount
+          }
+          redis.publish(channel_name, JSON.stringify(wsob))
+        }
+      })
+    }
+    function tickerEvent (args,kwargs) {
+    }
+    function trollboxEvent (args,kwargs) {
+    }
+
+    session.subscribe('BTC_ETH', marketEvent);
+    //session.subscribe('ticker', tickerEvent);
+    //session.subscribe('trollbox', trollboxEvent);
+  }
+
+
+
+  // Bitfinex pump
+  bws.on('open', function () {
+    bws.subscribeOrderBook('ETHBTC');
+
+    //bws.subscribeTrades('ETHBTC');
+    //bws.subscribeTicker('LTCBTC');
+  });
+
+  bws.on('close', function (pair, trade) {
+      console.log('btfx closed.', trade);
+  });
+
+  bws.on('trade', function (pair, trade) {
+      console.log('Trade:', trade);
+  });
+
+  bws.on('orderbook', function (pair, book) {
+    console.log('BTFX', book);
+    wsob = {
+      exchange: 'bitfinex',
+      market: 'ETH:BTC',
+      type:   book.amount > 0 ? "bid" : "ask",
+      price:  ""+book.price, // str to float to str, crosses fingers
+      amount: book.count == 0 ? "0" : (""+Math.abs(book.amount))
+    }
+    redis.publish(channel_name, JSON.stringify(wsob))
+  });
+
+  bws.on('ticker', function (pair, ticker) {
+      console.log('Ticker:', ticker);
+  });
+
+  bws.on('subscribed', function (data) {
+      console.log('New subscription', data);
+  });
+
+  bws.on('error', console.error);
+}
+
 
 function bleu_refresh(){
   request.get('https://bleutrade.com/api/v2/public/getorderbook?type=ALL&market=ETH_BTC', function (error, response, body) {
@@ -115,8 +127,6 @@ function bleu_refresh(){
   })
 }
 
-// Kraken pump
-setInterval(kraken_refresh, 5000)
 
 function kraken_refresh(){
   request.get('https://api.kraken.com/0/public/Depth?pair=XETHXXBT&count=20', function (error, response, body) {
