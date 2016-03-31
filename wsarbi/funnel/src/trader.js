ed51f0965c9e878d6bb8402677af41a51b974cbc
Don Park <donp@donp.org>
2016-03-31 11:49:08 -0700

    kraken bits

--- wsarbi/funnel/src/trader.js
+++ wsarbi/funnel/src/trader.js
@@ -5,29 +5,39 @@ let fs = require('fs')
 // npm
 let request = require('request')
 
+// config
+let config = JSON.parse(fs.readFileSync(__dirname+'/../../config.json'))
+
 // Exchanges
 let bitfinex = require('bitfinex-api-node');
 let poloniex = require('plnx')
+let krakenClient = require('kraken-api')
+var kraken = new krakenClient(config.exchanges.kraken.key,
+                              config.exchanges.kraken.secret)
 
 // Redis
 let redis = require('redis').createClient()
 
-// config
-console.log()
-let config = JSON.parse(fs.readFileSync(__dirname+'/../../config.json'))
 let balances = {}
 
 console.log('trader')
+
+console.log('poloniex balance load')
 let creds = config.exchanges.poloniex
 poloniex.returnCompleteBalances({ key: creds.key, secret: creds.secret }, function(err, data) {
   if(!err) {
     Object.keys(data).forEach(function(currency){
       let money = data[currency]
       if(money.available > 0) {
-        console.log(currency, money)
+        console.log('poloniex', currency, money)
       }
     })
   } else {
-    console.log(err, data);
+    console.log('poloniex', err, data);
   }
 });
+
+console.log('kraken balance load')
+kraken.api('Balance', null, function(error, data) {
+  console.log('kraken', error, data)
+})
