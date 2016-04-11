e7e496722fd87864d6670561f0bcc0f5859950d3
Don Park <donp@donp.org>
2016-04-11 10:33:15 -0700

    standard-format

--- wsarbi/funnel/src/funnel.js
+++ wsarbi/funnel/src/funnel.js
@@ -1,15 +1,17 @@
-var BitfinexWS = require('bitfinex-api-node').WS;
+// public APIs are all http
 var request = require('request')
 
-var autobahn = require('autobahn');
+// websocket specific support
+var BitfinexWS = require('bitfinex-api-node').WS
+var autobahn = require('autobahn')
 var poloniex_stream = new autobahn.Connection({
-  url: "wss://api.poloniex.com",
-  realm: "realm1"
-});
+  url: 'wss://api.poloniex.com',
+  realm: 'realm1'
+})
 
+// redis common bus
 var redis = require('redis').createClient()
 var channel_name = 'orderbook'
-
 
 // buggy delay
 // setTimeout(stream_setup, 15000)
@@ -23,161 +25,155 @@ setInterval(poloniex_refresh, 5000)
 // Bitfinex pump
 setInterval(bitfinex_refresh, 5000)
 
-
-function stream_setup() {
-
+function stream_setup () {
   // poloniex pump
   poloniex_stream.onopen = function (session) {
     function marketEvent (args, kwargs) {
-      args.forEach(function(ob){
-        if(ob.type == "orderBookModify" || ob.type == "orderBookRemove") {
-          console.log("POLO", ob.type, ob.data);
-          wsob = {
+      args.forEach(function (ob) {
+        if (ob.type === 'orderBookModify' || ob.type === 'orderBookRemove') {
+          console.log('POLO', ob.type, ob.data)
+          let wsob = {
             exchange: 'poloniex',
             market: 'ETH:BTC',
-            type:   ob.data.type,
-            price:  ob.data.rate,
-            amount: ob.type == "orderBookRemove" ? "0" : ob.data.amount
+            type: ob.data.type,
+            price: ob.data.rate,
+            amount: ob.type === 'orderBookRemove' ? '0' : ob.data.amount
           }
           redis.publish(channel_name, JSON.stringify(wsob))
         }
       })
     }
-    function tickerEvent (args,kwargs) {
+    function tickerEvent (args, kwargs) {
     }
-    function trollboxEvent (args,kwargs) {
+    function trollboxEvent (args, kwargs) {
     }
 
-    session.subscribe('BTC_ETH', marketEvent);
-    //session.subscribe('ticker', tickerEvent);
-    //session.subscribe('trollbox', trollboxEvent);
+    session.subscribe('BTC_ETH', marketEvent)
+  // session.subscribe('ticker', tickerEvent)
+  // session.subscribe('trollbox', trollboxEvent)
   }
 
   // start here because reasons
   poloniex_stream.open()
 
   // starts automatically. boo.
-  var bws = new BitfinexWS();
+  var bws = new BitfinexWS()
   // Bitfinex pump
   bws.on('open', function () {
-    bws.subscribeOrderBook('ETHBTC');
+    bws.subscribeOrderBook('ETHBTC')
 
-    //bws.subscribeTrades('ETHBTC');
-    //bws.subscribeTicker('LTCBTC');
-  });
+  // bws.subscribeTrades('ETHBTC')
+  // bws.subscribeTicker('LTCBTC')
+  })
 
   bws.on('close', function (pair, trade) {
-      console.log('btfx closed.', trade);
-  });
+    console.log('btfx closed.', trade)
+  })
 
   bws.on('trade', function (pair, trade) {
-      console.log('Trade:', trade);
-  });
+    console.log('Trade:', trade)
+  })
 
   bws.on('orderbook', function (pair, book) {
-    console.log('BTFX', book);
-    wsob = {
+    console.log('BTFX', book)
+    let wsob = {
       exchange: 'bitfinex',
       market: 'ETH:BTC',
-      type:   book.amount > 0 ? "bid" : "ask",
-      price:  ""+book.price, // str to float to str, crosses fingers
-      amount: book.count == 0 ? "0" : (""+Math.abs(book.amount))
+      type: book.amount > 0 ? 'bid' : 'ask',
+      price: '' + book.price, // str to float to str, crosses fingers
+      amount: book.count === 0 ? '0' : ('' + Math.abs(book.amount))
     }
     redis.publish(channel_name, JSON.stringify(wsob))
-  });
+  })
 
   bws.on('ticker', function (pair, ticker) {
-      console.log('Ticker:', ticker);
-  });
+    console.log('Ticker:', ticker)
+  })
 
   bws.on('subscribed', function (data) {
-      console.log('New subscription', data);
-  });
+    console.log('New subscription', data)
+  })
 
-  bws.on('error', console.error);
+  bws.on('error', console.error)
 }
 
-
-function bleu_refresh(){
+function bleu_refresh () {
   exchange_reset('bleutrade',
     'https://bleutrade.com/api/v2/public/getorderbook?type=ALL&market=ETH_BTC',
-    function(answer) {
+    function (answer) {
       return {
-        market: "ETH:BTC",
+        market: 'ETH:BTC',
         bids: answer.result.buy,
         asks: answer.result.sell
       }
     },
-    function(offer){
+    function (offer) {
       return {
-        price:  offer.Rate,
+        price: offer.Rate,
         amount: offer.Quantity
       }
-  })
+    })
 }
 
-
-function kraken_refresh(){
+function kraken_refresh () {
   exchange_reset('kraken',
     'https://api.kraken.com/0/public/Depth?pair=XETHXXBT&count=50',
-    function(answer) {
+    function (answer) {
       return {
-        market: "ETH:BTC",
+        market: 'ETH:BTC',
         bids: answer.result.XETHXXBT.bids,
         asks: answer.result.XETHXXBT.asks
       }
     },
-    function(offer){
+    function (offer) {
       return {
-        price:  offer[0],
+        price: offer[0],
         amount: offer[1]
       }
-  })
+    })
 }
 
-
-function poloniex_refresh(){
+function poloniex_refresh () {
   exchange_reset('poloniex',
     'https://poloniex.com/public?command=returnOrderBook&currencyPair=BTC_ETH',
-    function(answer) {
+    function (answer) {
       return {
-        market: "ETH:BTC",
+        market: 'ETH:BTC',
         bids: answer.bids,
         asks: answer.asks
       }
     },
-    function(offer){
+    function (offer) {
       return {
-        price:  offer[0],
-        amount: ""+offer[1]
+        price: offer[0],
+        amount: '' + offer[1]
       }
-  })
+    })
 }
 
-
-function bitfinex_refresh(){
+function bitfinex_refresh () {
   exchange_reset('bitfinex',
     'https://api.bitfinex.com/v1/book/ETHBTC',
-    function(answer) {
+    function (answer) {
       return {
-        market: "ETH:BTC",
+        market: 'ETH:BTC',
         bids: answer.bids,
         asks: answer.asks
       }
     },
-    function(offer){
+    function (offer) {
       return {
-        price:  offer.price,
+        price: offer.price,
         amount: offer.amount
       }
-  })
+    })
 }
 
-function exchange_reset(name, api_url, answer_morph, offer_morph) {
+function exchange_reset (name, api_url, answer_morph, offer_morph) {
   console.log(api_url)
   request.get(api_url, function (error, response, body) {
     var load = {
-      type: "load",
+      type: 'load',
       exchange: name
     }
 
@@ -187,18 +183,16 @@ function exchange_reset(name, api_url, answer_morph, offer_morph) {
       var book = answer_morph(response)
       load['market'] = book.market
       console.log(name, 'top bid', JSON.stringify(book.bids[0]),
-                        'top ask', JSON.stringify(book.asks[0]))
+        'top ask', JSON.stringify(book.asks[0]))
 
-      var sides = [ "bid", "ask" ]
-      sides.forEach(function(side){
-        load[side+'s'] = book[side+'s'].map(offer_morph)
+      var sides = [ 'bid', 'ask' ]
+      sides.forEach(function (side) {
+        load[side + 's'] = book[side + 's'].map(offer_morph)
       })
 
       redis.publish(channel_name, JSON.stringify(load))
     } catch (e) {
-      console.log(name, 'JSON ERR', e, body ? body.substr(0,100) : "empty body")
+      console.log(name, 'JSON ERR', e, body ? body.substr(0, 100) : 'empty body')
     }
   })
 }
-
-
--- wsarbi/funnel/src/trader.js
+++ wsarbi/funnel/src/trader.js
@@ -1,21 +1,22 @@
-"use strict"
+'use strict'
 // system
 let fs = require('fs')
+let path = require('path')
 
 // npm
 let request = require('request')
 
 // config
-let config = JSON.parse(fs.readFileSync(__dirname+'/../../config.json'))
+let config = JSON.parse(fs.readFileSync(path.join(__dirname, '/../../config.json')))
 
 // Exchanges
-let bitfinexClient = require('bitfinex-api-node').APIRest
-var bitfinex = new bitfinexClient(config.exchanges.bitfinex.key,
-                                  config.exchanges.bitfinex.secret)
+let Bitfinex = require('bitfinex-api-node').APIRest
+var bitfinex = new Bitfinex(config.exchanges.bitfinex.key,
+  config.exchanges.bitfinex.secret)
 let poloniex = require('plnx')
-let krakenClient = require('kraken-api')
-var kraken = new krakenClient(config.exchanges.kraken.key,
-                              config.exchanges.kraken.secret)
+let Kraken = require('kraken-api')
+var kraken = new Kraken(config.exchanges.kraken.key,
+  config.exchanges.kraken.secret)
 
 // Redis
 let redis = require('redis').createClient()
@@ -26,32 +27,31 @@ console.log('trader')
 
 console.log('poloniex balance load')
 let creds = config.exchanges.poloniex
-poloniex.returnCompleteBalances({ key: creds.key, secret: creds.secret }, function(err, data) {
-  if(!err) {
-    Object.keys(data).forEach(function(currency){
+poloniex.returnCompleteBalances({ key: creds.key, secret: creds.secret }, function (err, data) {
+  if (!err) {
+    Object.keys(data).forEach(function (currency) {
       let money = data[currency]
-      if(money.available > 0) {
+      if (money.available > 0) {
         console.log('poloniex', currency, money)
       }
     })
   } else {
-    console.log('poloniex', err, data);
+    console.log('poloniex', err, data)
   }
 })
 
 console.log('kraken balance load')
-kraken.api('Balance', null, function(error, data) {
+kraken.api('Balance', null, function (error, data) {
   console.log('kraken', error, data)
 })
-kraken.api('OpenOrders', null, function(error, data) {
+kraken.api('OpenOrders', null, function (error, data) {
   console.log('kraken', error, data)
 })
-
 
 console.log('bitfinex balance load')
-bitfinex.wallet_balances(function(error, balances){
+bitfinex.wallet_balances(function (error, balances) {
   console.log('bitfinex', error, balances)
 })
-bitfinex.active_orders(function(error, orders){
+bitfinex.active_orders(function (error, orders) {
   console.log('bitfinex', error, orders)
 })
