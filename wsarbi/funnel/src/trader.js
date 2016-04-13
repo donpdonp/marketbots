b5d4a06d6072845306543867ae5fefc4584b7c6f
Don Park <donp@donp.org>
2016-04-13 15:44:38 -0700

    balance checking and email notices

--- wsarbi/funnel/src/trader.js
+++ wsarbi/funnel/src/trader.js
@@ -2,9 +2,12 @@
 // system
 let fs = require('fs')
 let path = require('path')
+let os = require('os')
 
 // npm
 let request = require('request')
+let nodemailer = require('nodemailer')
+let mailer = nodemailer.createTransport()
 
 // config
 let config = JSON.parse(fs.readFileSync(path.join(__dirname, '/../../config.json')))
@@ -20,8 +23,10 @@ let redis = require('redis').createClient()
 console.log('trader')
 
 key_master().then(function (keys) {
-  balance_master(keys)
-  plan_listen()
+  let balances = new Map()
+
+  balance_master(keys, balances)
+  plan_listen(balances)
 }, function (err) {
   console.log('auth failed', err)
 })
@@ -56,10 +61,9 @@ function key_master () {
     })
 }
 
-function balance_master (creds) {
-  let balances = new Map()
+function balance_master (creds, balances) {
   for (let exchange in creds) {
-    balances[exchange] = {}
+    balances[exchange] = new Map([['fresh', false]])
   }
   console.log('poloniex balance load')
   poloniex.returnCompleteBalances(
@@ -69,6 +73,7 @@ function balance_master (creds) {
         balances.poloniex.btc = data['BTC'].available
         balances.poloniex.eth = data['ETH'].available
         console.log('poloniex', 'btc', balances.poloniex.btc, 'eth', balances.poloniex.eth)
+        balances.poloniex.fresh = true
       } else {
         console.log('poloniex', err, data)
       }
@@ -78,11 +83,17 @@ function balance_master (creds) {
   console.log('kraken balance load')
   kraken.api('Balance', null, function (error, data) {
     console.log('kraken balance', error, data)
-    if (data.result.btc) {
-      balances.kraken = data.result.btc
+    if (data.result.XETH) {
+      balances.kraken.eth = data.result.XETH
     } else {
-      balances.kraken = 0
+      balances.kraken.eth = 0
     }
+    if (data.result.BTC) {
+      balances.kraken.btc = data.result.BTC
+    } else {
+      balances.kraken.btc = 0
+    }
+    balances.kraken.fresh = true
     console.log('kraken', 'btc', balances.kraken.btc, 'eth', balances.kraken.eth)
   })
   console.log('kraken order load')
@@ -101,25 +112,62 @@ function balance_master (creds) {
   })
 }
 
-function plan_listen () {
+function plan_listen (balances) {
   redis.brpop('wsarbi:plan', 0, function (error, data) {
     if (error) {
       console.log('redis plan error!')
     } else {
-      console.log('NEW PLAN')
-      plan_execute(JSON.parse(data[1]))
+      console.log('NEW PLAN RECEIVED')
+      plan_execute(JSON.parse(data[1]), balances)
     }
   })
 }
 
-function plan_execute (plan) {
+function plan_execute (plan, balances) {
   // check plan freshness
 
   // check balances
   Object.keys(plan).forEach(function (epair) {
-    console.log('TRADE', epair)
+    let order = plan[epair]
+    console.log(epair, order)
+    let exs = epair.split(':')
+    let aske = balances[exs[0]]
+    let bide = balances[exs[1]]
+
+    let alert
+    if (aske && bide) {
+      alert = 'TRADE ' + epair + '\n'
+      alert += '  ask balance ' + aske.fresh + aske.btc + 'btc' + '\n'
+      alert += '  bid balance ' + bide.fresh + bide.btc + 'etc' + '\n'
+      if (aske.fresh && bide.fresh) {
+        alert += '  balances FRESH' + '\n'
+        let btc_amount = order['amount'] * 1
+        let btc_spend = Math.min(btc_amount, aske.btc)
+        alert += '  btc_spend ' + btc_spend + '\n'
+        let eth_spend = Math.min(order['amount'], bide.eth)
+        alert += '  eth_spend ' + eth_spend + '\n'
+      }
+    } else {
+      alert = 'error missing balances' + JSON.stringify(exs) + '\n'
+    }
+    email(alert)
   })
 
   // play it again, sam.
-  plan_listen()
+  plan_listen(balances)
+}
+
+function email (text) {
+  let email = {
+    from: config.email.from, // sender address
+    to: config.email.to, // list of receivers
+    subject: os.hostname() + ' plan execute',
+    text: text
+  }
+  mailer.sendMail(email, function (error, info) {
+    if (error) {
+      return console.log(error)
+    }
+    console.log('Message sent: ', info)
+  })
 }
